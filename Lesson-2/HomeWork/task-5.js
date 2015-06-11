7abd084c1b621983165962225f03a72cacfde802
Saleksei <info@mlight.com.ua>
2015-06-11 19:28:06 +0300

    Lesson 2, homework

--- Lesson-2/HomeWork/task-5.js
+++ Lesson-2/HomeWork/task-5.js
@@ -7,5 +7,6 @@ var name = 'admin', text;
 
 name == 'admin' ? text = 'Hi!'  :
     name == 'manager' ? text = 'Hello!' :
-        name == '' ? text = '';
+        name == '' ? text = '' : null;
 
+console.log(text);
