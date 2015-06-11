1e345ccaabbf249f4632f4225123c74357d628c4
Saleksei <info@mlight.com.ua>
2015-06-11 16:52:36 +0300

    Lesson 2, homework

---
+++ Lesson-2/HomeWork/task-5.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Алексей on 11.06.2015.
+ */
+/**Перепишите if..else с использованием нескольких операторов '?':*/
+
+var name = 'admin', text;
+
+name == 'admin' ? text = 'Hi!'  :
+    name == 'manager' ? text = 'Hello!' :
+        name == '' ? text = '';
+
